2c5bb6b644f093d11f39feed1c7d7c881de0bd2a
QC20 <xgv866@alumni.ku.dk>
2024-06-25 11:01:08 +0200

    repo refractor and change noodle and stick motion

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -72,7 +72,7 @@ let noodlesDropped = 0;
 // Count the number of noodles that were initially created
 let noodlesCount = 0;
 // Define how many noodles will be on screen
-let noodlesMax = 16;
+let noodlesMax = 14;
 // Equal the noodle timeout to the length of the noodles to keep world object totals in balance
 let noodleTimeout = noodleLength;
 
@@ -92,7 +92,7 @@ const noodleRestrictor = function() {
 // Define the function for adding a new noodle
 const noodle = function() {
   // Create a random number to vary the position of where the noodle will be dropped from
-  let random = Math.floor(Math.random() * 100 - 50);
+  let random = Math.floor(Math.random() * 100);
   // Create a bit of variation in noodle appearance
   let type = noodle0;
   // If the number is divisible by 2 then change the appearance
@@ -131,8 +131,8 @@ const noodle = function() {
 };
 
 let chopstickLength = width / 2;
-if (chopstickLength > 500) {
-  chopstickLength = 500;
+if (chopstickLength > 600) {
+  chopstickLength = 600;
 }
 
 // Create some chopsticks to interact with the noodles
@@ -179,7 +179,7 @@ Matter.Events.on(engine, "afterUpdate", function() {
     return;
   }
   // Limit the x axis positioning of the chopsticks to keep them on the right hand side of the screen
-  const limit = width / 2 + chopstickLength / 3;
+  const limit = width / 1.875 + chopstickLength / 3;
   let position = mouse.position.x;
   if (mouse.position.x < limit) {
     position = limit;
@@ -194,11 +194,11 @@ Matter.Events.on(engine, "afterUpdate", function() {
     y: width * 0.04 + 5
   };
   Body.translate(chopstick1, {
-    x: (position - chopstick1.position.x - offset1.x) * 0.1,
+    x: (position - chopstick1.position.x - offset1.x) * 0.35,
     y: (mouse.position.y - chopstick1.position.y + offset1.y) * 0.1
   });
   Body.translate(chopstick2, {
-    x: (position - chopstick2.position.x - offset2.x) * 0.1,
+    x: (position - chopstick2.position.x - offset2.x) * 0.2,
     y: (mouse.position.y - chopstick2.position.y + offset2.y) * 0.1
   });
 });
@@ -209,8 +209,8 @@ let addNoodles = true;
 // Define a mouse click event
 window.addEventListener("mousedown", function() {
   // Move the chopsticks together
-  Body.setAngle(chopstick1, -Math.PI * 0.075);
-  Body.setAngle(chopstick2, -Math.PI * 0.055);
+  Body.setAngle(chopstick1, -Math.PI * 0.080);
+  Body.setAngle(chopstick2, -Math.PI * 0.050);
 });
 
 // Define the mouse click release event
